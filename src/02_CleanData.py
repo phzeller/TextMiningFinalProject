67b5614afa6ace414bca43138c82406fa2df448b
phzeller <i.am@philippzeller.com>
2022-03-05 19:30:36 +0100

    Created classes for two major steps: 1) clean data (pre-processing) 2) addition of financial data to the dataframe
    Furthermore, changed FomcMinutes in order to scrape data until 2022. However, functionality does not work yet.

---
+++ src/02_CleanData.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+minutes = pd.read_pickle('../data/FOMC/minutes.pickle')
+
+# Data cleaning:
+# Tokenization, cleaning, normalization, stemming, stop-word removal, lemmatization (create new column for cleaned data)
+
+
+
+
---
+++ src/03_AddFinancialData.py
@@ -0,0 +1,1 @@
+# Adding data of pctchange to the days after the announcement (14 days; for each day one column)
