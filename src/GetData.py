3456dd5c742078411d94267589ff11558f114ee3
phzeller <i.am@philippzeller.com>
2022-03-05 17:33:06 +0100

    Introduced GetData.py in order to gather all the important data (interest rates, FED minutes, and S&P500 historical data)

---
+++ src/GetData.py
@@ -0,0 +1,18 @@
+# Data sources
+
+import yfinance as yf
+import pandas as pd
+from fomc_get_data.FomcMinutes import FomcMinutes
+
+# Get history of interest rate (10 Years Treasury Yield)
+TNX = yf.Ticker('^TNX')
+TNX_hist = TNX.history(period='max')
+
+# Get history of S&P 500
+SP500 = yf.Ticker('SPY')
+SP500_hist = SP500.history(period='max')
+
+# Get FED minutes
+fomc = FomcMinutes()
+df_minutes = fomc.get_contents()
+
