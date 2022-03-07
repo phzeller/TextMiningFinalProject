a86d3f7cd08137edd4409d155d539045bd6188c8
phzeller <i.am@philippzeller.com>
2022-03-07 17:17:16 +0100

    Removal of test file

--- src/test_minutes pickle.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import pickle
-
-
-with open(r"C:\Users\philippz\PycharmProjects\TextMiningFinalProject\data\FOMC\minutes.pickle" , 'rb') as f:
-    df=pickle.load(f)
-
-print(df.i['contents'])
